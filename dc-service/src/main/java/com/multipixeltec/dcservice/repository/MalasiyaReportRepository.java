82228b36396b34b3bc3baba00894623f220aaa12
deki08 <teamsoul234@gmail.com>
2023-05-26 00:30:14 +0530

    3rd and 2nd

---
+++ dc-service/src/main/java/com/multipixeltec/dcservice/repository/MalasiyaReportRepository.java
@@ -0,0 +1,11 @@
+package com.multipixeltec.dcservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.multipixeltec.dcservice.model.MalasiyaReport;
+
+public interface MalasiyaReportRepository extends JpaRepository<MalasiyaReport, Long> {
+
+	MalasiyaReport findByPatientId(Long long1);
+
+}
